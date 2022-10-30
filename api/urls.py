7107d832278d9a002064b7ee44a6159a82cd4c84
Mohammadmahdi-Mokabberi <mm.mokabberi@gmail.com>
2022-10-30 14:45:50 +0330

    update

--- api/urls.py
+++ api/urls.py
@@ -3,9 +3,9 @@ from .views import LoginAPIView, RegisterAPIView, GetScoreAPIView, CreateArticle
 
 
 urlpatterns = [
-    path('register/', RegisterAPIView.as_view(),),
-    path('login/', LoginAPIView.as_view(),),
-    path('score-blog/<str:pk>', GetScoreAPIView.as_view(),),
-    path('create-article/', CreateArticleAPIView.as_view(),),
-    path('article-list/', ArticleListAPIView.as_view()),
+    path('user/register/', RegisterAPIView.as_view(),),
+    path('user/login/', LoginAPIView.as_view(),),
+    path('article/score/<str:pk>', GetScoreAPIView.as_view(),),
+    path('article/create/', CreateArticleAPIView.as_view(),),
+    path('article/list/', ArticleListAPIView.as_view()),
 ]
